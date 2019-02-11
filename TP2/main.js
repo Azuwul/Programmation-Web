d58c6677d93394ce8ef750e1e38d8de27947601c
Alex Volcwiartek <44714879+Azuwul@users.noreply.github.com>
2019-02-11 19:43:00 +0100

    Génère aléatoirement 50 identités (prénom, nom, age, ...)

--- TP2/main.js
+++ TP2/main.js
@@ -1,6 +1,6 @@
 var nbrRepetion = 50;
 for(var i=0; i<nbrRepetion; i++){
-	var list = "<tr><td> " + faker.name.firstName() + "</td><td>" + faker.name.lastName() + "</td><td>" + faker.internet.email() + "</td><td>" + (Math.floor(Math.random() * (67 - 19 + 1) ) + 19) + "</td><td>" + faker.address.streetAddress() + "</td><td>" + faker.name.jobTitle() + "</td><td>" + faker.phone.phoneNumberFormat() + "</td></tr>";
+	var list = "<tr><td> " + faker.name.firstName() + "</td><td>" + faker.name.lastName() + "</td><td>" + (Math.floor(Math.random() * (67 - 19 + 1) ) + 19) + "</td><td>" + faker.address.streetAddress() + "</td><td>" + faker.name.jobType() + "</td><td>" + faker.phone.phoneNumberFormat() + "</td></tr>";
 	var t = document.getElementById("tst");
 	t.innerHTML += list;
 }
